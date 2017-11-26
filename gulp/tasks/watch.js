eddda13b4efc3c348fd7c7d8f3fc2c5bc057dd7f
siberianLvolk <callmevolk@gmail.com>
2017-11-26 17:58:51 +0100

    Ready for javascript

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,6 +19,14 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  });
+
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
 
 gulp.task('cssInject', ['styles'], function() {
